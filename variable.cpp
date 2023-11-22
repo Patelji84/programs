3a019d18aa219c1bc8a8806a0194a2af72ddf705
Patelji84 <122530706+Patelji84@users.noreply.github.com>
2023-11-22 22:40:12 +0530

    Add files via upload

---
+++ variable.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char Yes ='c';
+     cout<<Yes ;
+
+
+// The (<<) operator can be used multiple times in a single statement
+
+cout<<" hello\n"<<"this is a test \n"<<"String";
+
+
+
+
+return 0;
+
+
+
+
+
+}
